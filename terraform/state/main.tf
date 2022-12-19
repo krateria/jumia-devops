da7ad51d03260654f7d2d43a6a7aa442ca72d045
Fernando La Gamba <krateria@gmail.com>
2022-12-19 00:04:06 -0300

    update

--- terraform/state/main.tf
+++ terraform/state/main.tf
@@ -33,7 +33,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "ionicdevopsterraform-up-and-running-state"
+  name         = "jumia-devops-tf-up-and-running-state"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
